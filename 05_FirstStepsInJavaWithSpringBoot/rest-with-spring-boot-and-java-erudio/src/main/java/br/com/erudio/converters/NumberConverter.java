1f2b9cd91d0a1f4d22538c5d0d9011a6aae7f30e
Welton Mota <welton.g.motta@gmail.com>
2022-07-29 12:01:23 -0300

    Parameters And Exception Handler part 2

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/converters/NumberConverter.java
@@ -0,0 +1,18 @@
+package br.com.erudio.converters;
+
+public class NumberConverter {
+	
+	public static Double convertToDouble(String strNumber) {
+		if (strNumber == null) return 0D;
+		// BRL 10,25 USD 10.25
+		String number = strNumber.replaceAll(",", ".");
+		if (isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
+	}
+
+	public static boolean isNumeric(String strNumber) {
+		if (strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+}
